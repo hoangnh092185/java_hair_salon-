6b0c1561c67274259efa523830784f488639046a
nhat hoang <hoangnh092185@gmail.com>
2016-09-30 16:52:14 -0700

    fix test errors.

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -9,23 +9,23 @@ public class StylistTest{
 
   @Test
   public void equals_returnsTrueIfNameAreTheSame(){
-    Stylist newStylist1 = new Stylist("Nhat");
-    Stylist newStylist2 = new Stylist("Nhat");
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
+    Stylist newStylist2 = new Stylist("Nhat", "I love hair", "3");
     assertTrue(newStylist1.equals(newStylist2));
   }
 
   @Test
   public void save_returnsSaveStylist_true(){
-    Stylist newStylist1 = new Stylist("Nhat");
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
     newStylist1.save();
     assertEquals(newStylist1, Stylist.all().get(0));
   }
 
   @Test
-  public void all_returnsAllinstancesOfPerson_true(){
-    Stylist newStylist1 = new Stylist("Nhat");
+  public void all_returnsAllinstancesOfPerRobbin_true(){
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
     newStylist1.save();
-    Stylist newStylist2 = new Stylist("Nate");
+    Stylist newStylist2 = new Stylist("Nate", "I realy love hair", "3");
     newStylist2.save();
     assertEquals(true, Stylist.all().get(0).equals(newStylist1));
     assertEquals(true, Stylist.all().get(1).equals(newStylist2));
@@ -33,7 +33,7 @@ public class StylistTest{
 
   @Test
   public void save_assignsIdToObject(){
-    Stylist newStylist1 = new Stylist("Nhat");
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
     newStylist1.save();
     Stylist saveStylist = Stylist.all().get(0);
     assertEquals(newStylist1.getId(), saveStylist.getId());
@@ -41,27 +41,27 @@ public class StylistTest{
 
   @Test
   public void getId_stylistsInstantiateWithId(){
-    Stylist newStylist1 = new Stylist("Nhat");
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
     newStylist1.save();
     assertTrue(newStylist1.getId()>0);
   }
 
   @Test
   public void find_returnsStylistWithSameId_newStylist2(){
-    Stylist newStylist1 = new Stylist("Nhat");
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
     newStylist1.save();
-    Stylist newStylist2 = new Stylist("Nate");
+    Stylist newStylist2 = new Stylist("Nate", "I realy love hair", "3");
     newStylist2.save();
     assertEquals(Stylist.find(newStylist2.getId()), newStylist2);
   }
 
   @Test
   public void getClients_returnClients_list(){
-    Stylist newStylist1 = new Stylist("Nhat");
+    Stylist newStylist1 = new Stylist("Nhat", "I love hair", "3");
     newStylist1.save();
     Client newClient1 = new Client("Batman", newStylist1.getId());
     newClient1.save();
-    Client newClient2 = new Client("Son", newStylist1.getId());
+    Client newClient2 = new Client("Robbin", newStylist1.getId());
     newClient2.save();
     assertEquals(newStylist1.getClients().get(0), newClient1);
   }
